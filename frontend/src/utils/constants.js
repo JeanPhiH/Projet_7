4e0a0d350aa951df8d661ba334e3841f33e6d20a
JeanPhiH <jp.herfeld@gmail.com>
2024-04-29 18:47:52 +0200

    testing API_URL

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 // const API_URL = 'http://localhost:4000';
-const API_URL = 'projet-7-e2tutb9nk-jeanphihs-projects.vercel.app:4000';
+const API_URL = 'https://projet-7-six.vercel.app';
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
