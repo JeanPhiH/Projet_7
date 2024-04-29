13336590d09f1b605946ac90b2939f6c8d4dac74
JeanPhiH <jp.herfeld@gmail.com>
2024-04-29 18:30:54 +0200

    testing vercel deployment

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:4000';
+// const API_URL = 'http://localhost:4000';
+const API_URL = 'projet-7-e2tutb9nk-jeanphihs-projects.vercel.app';
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
