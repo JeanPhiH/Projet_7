8b3b60921df9be96b8fae68bd0b94d64149af772
JeanPhiH <jp.herfeld@gmail.com>
2024-04-30 18:12:33 +0200

    add readme file + sharp comment

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mon vieux Grimoire
+_Projet 7 de la formation Développeur Web d'OpenClassrooms._
+
+## Comment lancer le projet ? 
+
+### Pour le frontend
+`npm install` puis `npm run start` pour lancer le projet React. 
+
+### Pour le backend
+`npm install` puis `npm run start` pour lancer le serveur Node.
+
--- backend/middleware/sharp.js
+++ backend/middleware/sharp.js
@@ -8,9 +8,8 @@ module.exports = async (req, res, next) => {
 		return next()
 	};
 	const filePath = req.file.path;
-	const { name: resizedName } = path.parse(filePath);
-	req.file.resizedFileName = `${resizedName}-rs.webp`;
-	// add '-rs' to handle webp images deletion
+	const { name: resizedName } = path.parse(filePath); // to avoid double extensions
+	req.file.resizedFileName = `${resizedName}-rs.webp`; // add '-rs' to handle webp images deletion
 
 	await sharp(filePath).resize({height: 500}).webp({ quality: 80 }).toFile("images/" + req.file.resizedFileName)
 	.catch((error) => console.log(error));
