b6151d6f3dab67b24b4da8c76318def89a064e1c
JeanPhiH <jp.herfeld@gmail.com>
2024-06-17 16:23:04 +0200

    trying another cors method

--- backend/app.js
+++ backend/app.js
@@ -34,14 +34,13 @@ const limiter = rateLimit({
 app.use(limiter); // Apply the rate limiting middleware to all requests.
 
 //CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://projet-7-frontend-eight.vercel.app');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://projet-7-frontend-eight.vercel.app');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
 const cors = require('cors');
-// Allow all origins
 app.use(cors());
 
 //SHORTCUTS
