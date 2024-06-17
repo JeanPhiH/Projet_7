4e9d52ea8c8bf07c628ef08d7eeabb560d702737
JeanPhiH <jp.herfeld@gmail.com>
2024-06-17 15:45:41 +0200

    Trying to fix cors

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use(limiter); // Apply the rate limiting middleware to all requests.
 
 //CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://projet-7-navy.vercel.app/*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
